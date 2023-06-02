d03bed250e99d893a5421506cdd3bab48f8d5385
Owolabi precious <owo.pre.eno@gmail.com>
2023-06-02 11:34:06 +0100

    home page

--- Frontend/src/app/page.js
+++ Frontend/src/app/page.js
@@ -1,8 +1,19 @@
+import Link from "next/link";
 import React from "react";
 
 const Page = () => {
-    return <div className="h-full">Page</div>
-    ;
+    return (
+        <div className="grid h-full w-full place-content-center ">
+            <div className="flex gap-x-8 items-center justify-center">
+                <Link href="/login/admin" className="border-accent border-2 px-8 rounded-md text-accent py-3">
+                    Login
+                </Link>
+                <Link href="/signup/admin" className="bg-accent px-8 rounded-md py-3">
+                    Signup
+                </Link>
+            </div>
+        </div>
+    );
 };
 
 export default Page;
